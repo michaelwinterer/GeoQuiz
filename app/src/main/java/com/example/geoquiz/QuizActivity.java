57af6c025af349ecc64244721113129719eebbaa
michaelwinterer <120957856+michaelwinterer@users.noreply.github.com>
2023-09-07 22:00:11 +0200

    Question Bank etc.

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -8,8 +8,11 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main); // this is just the xml file w/o the extension
 
         /* Question Text View */
@@ -81,13 +85,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
 
     private void checkAnswer(boolean userPressedTrue){
-        Toast toast = Toast.makeText(MainActivity.this,
+        Toast toast = Toast.makeText(QuizActivity.this,
                 userPressedTrue == mQuestionBank[mCurrentIndex].isAnswerTrue() ? R.string.correct_toast : R.string.incorrect_toast,
             Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.BOTTOM, 0, 0);
