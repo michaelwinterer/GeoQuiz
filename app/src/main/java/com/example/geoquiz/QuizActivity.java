8ee1f096a7f9d70be5aa6b1f84ac9e09302dd275
michaelwinterer <120957856+michaelwinterer@users.noreply.github.com>
2023-09-09 12:46:33 +0200

    Cheat Activity

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,6 +1,8 @@
 package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -98,7 +100,8 @@ public class QuizActivity extends AppCompatActivity {
         mCheatButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(intent);
             }
         });
 
